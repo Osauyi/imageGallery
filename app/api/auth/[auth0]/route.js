fe42a39c7dfb174b8affc292d0d9548310fe6e25
Osauyi <victorimafidon17@gmail.com>
2023-09-21 13:52:45 +0100

    added links

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -4,6 +4,7 @@ import { handleAuth } from '@auth0/nextjs-auth0';
 
 console.log('AUTH0_CLIENT_SECRET = ', process.env.AUTH0_CLIENT_SECRET)
 console.log('AUTH0_SECRET = ', process.env.AUTH0_SECRET)
+console.log('AUTH0_ISSUER_BASE_URL = ', process.env.AUTH0_ISSUER_BASE_URL)
 
 export const GET = handleAuth();
 
