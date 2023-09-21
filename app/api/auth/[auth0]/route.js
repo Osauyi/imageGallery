0fb276e52f0e9bbaca6f2f7f1948986b39e360e6
Osauyi <victorimafidon17@gmail.com>
2023-09-21 13:18:18 +0100

    checks

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,6 +1,7 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
 
+console.log('AUTH0_CLIENT_SECRET = ', process.env.AUTH0_CLIENT_SECRET)
 console.log('AUTH0_SECRET = ', process.env.AUTH0_SECRET)
 
 export const GET = handleAuth();
