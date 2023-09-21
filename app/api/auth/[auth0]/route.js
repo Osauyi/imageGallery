490943f8a762cc99c674c3a11dbf957d3d94d801
Osauyi <victorimafidon17@gmail.com>
2023-09-21 13:47:39 +0100

    added links

--- app/api/auth/[auth0]/route.js
+++ app/api/auth/[auth0]/route.js
@@ -1,7 +1,10 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
 
+
 console.log('AUTH0_CLIENT_SECRET = ', process.env.AUTH0_CLIENT_SECRET)
 console.log('AUTH0_SECRET = ', process.env.AUTH0_SECRET)
 
 export const GET = handleAuth();
+
+export const dynamic = 'force-dynamic'
